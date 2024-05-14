99c87a4f119da63b5e3f47be171f65265bf4d10a
Phung Luu Hoang Long <phungluuhoanglong@gmail.com>
2024-05-14 17:07:25 +0700

    first init

---
+++ test.py
@@ -0,0 +1,25 @@
+# a = 1
+# b = 1
+
+# print(1, 2, 3, 4, sep='-', end='_')
+
+'''
+comments
+'''
+#comment
+
+#ép kiểu số thực sang số nguyên
+
+# a = 100.234
+# print(a)
+
+# xem thông tin kiểu dữ liệu
+
+import math
+import math
+import sys
+
+a = 10
+print(math.sqrt(math.pi))
+# print(sys.int_info)
+# print(sys.float_info)
